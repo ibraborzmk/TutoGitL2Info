674efec869ab528cf06f08ec1312784ed57a0c54
Anwar Motassim <amotassi@pcalb-mm0000.univ-jfc.fr>
2023-02-01 11:54:39 +0100

    ajout: sortie de l'app à la fermeture de la fenêtre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,8 +1,10 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener{
 
 	public static void main(String[] args) {
 		new Application();
@@ -12,16 +14,57 @@ public class Application extends Frame {
 		super();
 		
 		System.out.println("Autre ligne");
-		
+		this.addWindowListener(this);
 		Modèle modl = new Modèle();
 		Contrôleur ctrl = new Contrôleur(modl); 
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
